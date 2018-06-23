672d1ec44db38c6989dd0d4b58bf2f1ce7e605c3
Blake Bottum <blakeplatypus@gmail.com>
2018-06-22 23:31:26 -0500

    Homepage, add page, color change, settings

---
+++ lib/main.dart
@@ -0,0 +1,476 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:ui';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:intl/intl.dart';
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:collection';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+
+int color;
+
+List<Color> colors = [
+  Colors.pink,
+  Colors.red,
+  Colors.deepOrange,
+  Colors.orange,
+  Colors.amber,
+  Colors.yellow,
+  Colors.lime,
+  Colors.lightGreen,
+  Colors.green,
+  Colors.teal,
+  Colors.cyan,
+  Colors.lightBlue,
+  Colors.blue,
+  Colors.indigo,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.blueGrey,
+  Colors.grey
+];
+
+SettingsInfo settings = new SettingsInfo();
+
+void main(){
+  settings.readData().then((list){
+    if(list==null||list.length!=1){
+      color = 17;
+      settings.writeData("0").then((f){
+        runApp(new DynamicTheme(
+          themedWidgetBuilder: (context, theme){
+            return new MaterialApp(
+                theme: theme,
+                home: new HomePage()
+            );
+          },
+          data: (brightness) => new ThemeData(fontFamily: "Poppins",brightness: Brightness.light, canvasColor: colors[color],buttonColor: Colors.black38),
+          defaultBrightness: Brightness.light,
+        ));
+      });
+    }else{
+      color = int.parse(list[0]);
+      runApp(new DynamicTheme(
+        themedWidgetBuilder: (context, theme){
+          return new MaterialApp(
+            theme: theme,
+            home: new HomePage()
+          );
+        },
+        data: (brightness) => new ThemeData(fontFamily: "Poppins",brightness: Brightness.light, canvasColor: colors[color],buttonColor: Colors.black38),
+        defaultBrightness: Brightness.light,
+      ));
+    }
+  });
+}
+
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+  final c = TextEditingController();
+
+  String input = "";
+
+  FocusNode f = new FocusNode();
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+          backgroundColor: Colors.transparent,
+          leading: new IconButton(
+              icon: new Icon(Icons.palette),
+              onPressed: (){
+                return showDialog<Null>(
+                    context: context,
+                    builder: (context){
+                      return new Container(
+                        // Specify some width
+                        width: MediaQuery.of(context).size.width * .7,
+                        child: new GridView.count(
+                            crossAxisCount: 3,
+                            childAspectRatio: 1.0,
+                            padding: const EdgeInsets.all(4.0),
+                            mainAxisSpacing: 4.0,
+                            crossAxisSpacing: 4.0,
+                            children: [
+                              new ColorSelection(0),
+                              new ColorSelection(1),
+                              new ColorSelection(2),
+                              new ColorSelection(3),
+                              new ColorSelection(4),
+                              new ColorSelection(5),
+                              new ColorSelection(6),
+                              new ColorSelection(7),
+                              new ColorSelection(8),
+                              new ColorSelection(9),
+                              new ColorSelection(10),
+                              new ColorSelection(11),
+                              new ColorSelection(12),
+                              new ColorSelection(13),
+                              new ColorSelection(14),
+                              new ColorSelection(15),
+                              new ColorSelection(16),
+                              new ColorSelection(17)
+                            ]
+                        ),
+                      );
+                    }
+                );
+              }
+          ),
+          actions: [
+            new IconButton(
+              icon: new Icon(Icons.settings),
+              onPressed: (){
+                Navigator.push(context,new MaterialPageRoute(builder: (context) => new Settings()));
+              }
+            )
+          ],
+        elevation: 0.0
+      ),
+      body: new Container(
+        child: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // ignore: conflicting_dart_import
+                new Text("PPoll",style: new TextStyle(fontSize:80.0*MediaQuery.of(context).size.width/375.0,fontWeight: FontWeight.w100)),
+                new Container(height: 75.0*MediaQuery.of(context).size.width/375.0,width:250.0*MediaQuery.of(context).size.width/375.0,child: new RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                  child: new Text("Create a poll",style: new TextStyle(fontSize:30.0,color:Colors.white70)),
+                  onPressed: (){
+                    Navigator.push(context,new MaterialPageRoute(builder: (context) => new CreatePoll()));
+                  },
+                )),
+                new Container(),new Container(),
+                new Container(width: 150.0,child: new Center(child: new TextField(
+                  style: new TextStyle(fontSize: 25.0,color:Colors.black),
+                  controller: c,
+                  maxLength: 4,
+                  textAlign: TextAlign.center,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    hintText: 'Code',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    counterText: "",
+                  ),
+                  onChanged:  (s){
+                    input = s;
+                  },
+                  focusNode: f
+                ))),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    new Container(height: 50.0,width: 100.0,child: new RaisedButton(
+                      child: new Text("View",style: new TextStyle(fontSize: 20.0,color:Colors.white70)),
+                      onPressed: (){
+
+                      }
+                    )),
+                    new Container(height: 50.0, width: 100.0,child: new RaisedButton(
+                      child: new Text("Vote",style: new TextStyle(fontSize: 20.0,color:Colors.white70)),
+                      onPressed: (){
+
+                      }
+                    )),
+                  ]
+                ),
+                new Container(),
+                new Container()
+              ]
+            )
+        )
+      )
+    );
+  }
+}
+
+class ColorSelection extends StatelessWidget{
+  final int index;
+
+  ColorSelection(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return new GridTile(
+        child: new GestureDetector(
+            child: new Container(
+                color: new Color(colors[index].value)
+            ),
+            onTapUp: (t){
+              color = index;
+              settings.writeData(color.toString());
+              DynamicTheme.of(context).setThemeData(new ThemeData(fontFamily: "Poppins",brightness: Brightness.light, canvasColor: colors[color],buttonColor:Colors.black38));
+              Navigator.of(context).pop();
+            }
+        )
+    );
+  }
+}
+
+class CreatePoll extends StatefulWidget{
+  @override
+  CreatePollState createState() => new CreatePollState();
+}
+
+class CreatePollState extends State<CreatePoll>{
+
+  int optionCount = 1;
+
+  List<Widget> list = [];
+
+  bool oneChoice = true;
+
+  bool perm = false;
+
+  static List<String> choices = [];
+
+  static String question = "";
+
+  ScrollController s = new ScrollController();
+
+  @override
+  void initState(){
+    super.initState();
+    question = "";
+    choices.clear();
+    list.add(new Option(1));
+    choices.length = 1;
+    list.add(new Container(height: 50.0,padding: EdgeInsets.only(left:30.0,right:30.0),child: new RaisedButton(
+        child: new ListTile(
+            leading: new Icon(Icons.add,color:Colors.white),
+            title: new Text("Add",style: new TextStyle(color:Colors.white))
+        ),
+        onPressed: (){
+          if(optionCount<20){
+            list.insert(list.length-1,new Option(++optionCount));
+            if(s.position.pixels>0.0){
+              s.jumpTo(s.position.pixels+50);
+            }
+            choices.length = optionCount;
+            setState((){});
+          }
+        }
+    )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Create a Poll",style: new TextStyle(color:Colors.white)),backgroundColor: Colors.black54),
+      body: new Container(
+        child: new Center(
+          child: new ListView(
+            controller: s,
+            children: [
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  new Text(""),
+                  new Container(height: 50.0,padding: EdgeInsets.only(left:30.0,right:30.0),child: new TextField(
+                    decoration: new InputDecoration(
+                      hintText: 'Question',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      counterText: "",
+                    ),
+                    onSubmitted: (s){
+                      question = s;
+                    }
+                  )),
+                  new Text(""),
+                  new Column(
+                    children: list
+                  ),
+                  new Container(height:40.0),
+                  new Container(
+                    padding: EdgeInsets.only(left:10.0,right:10.0),
+                    child: new Container(
+                      height: 50.0,
+                      color:Colors.black12,
+                      child: new Row(
+                          children: [
+                            new Text("One choice per user",style: new TextStyle(fontSize:17.0,color:Colors.white)),
+                            new Expanded(child: new Switch(
+                              value: oneChoice,
+                              onChanged: (s){
+                                setState((){oneChoice = s;});
+                              },
+                              activeColor: Colors.black
+                            ))
+                          ]
+                      )
+                    )
+                  ),
+                  new Container(
+                      padding: EdgeInsets.only(top:10.0,left:10.0,right:10.0),
+                      child: new Container(
+                          height: 50.0,
+                          color:Colors.black12,
+                          child: new Row(
+                              children: [
+                                new Text("Permanent",style: new TextStyle(fontSize:17.0,color:Colors.white)),
+                                new Expanded(child: new Switch(
+                                    value: perm,
+                                    onChanged: (s){
+                                      setState((){perm = s;});
+                                    },
+                                    activeColor: Colors.black
+                                ))
+                              ]
+                          )
+                      )
+                  ),
+                  new Container(height:30.0),
+                  new Container(height:60.0,width:200.0,child:new RaisedButton(
+                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                    child: new Text("Submit",style: new TextStyle(fontSize:25.0,color:Colors.white)),
+                    onPressed: (){
+                      if(question!=null && !choices.contains(null)){
+                        String key = "";
+                        Random r = new Random();
+                        for(int i = 0; i<4;i++){
+                          List<String> nums = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+                          key+=nums[r.nextInt(36)];
+                        }
+                        print(key);
+                        print(question);
+                        print(choices);
+                        print(oneChoice);
+                        return showDialog(
+                          context: context,
+                          builder: (context){
+                            return new AlertDialog(
+                              title:new Text("Success"),
+                              content:new Text("Your poll has been created with the code "+key),
+                              actions: [
+                                new RaisedButton(
+                                  child: new Text("Okay",style:new TextStyle(color: Colors.black)),
+                                  onPressed: (){
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pop();
+                                  },
+                                  color: Colors.grey
+                                )
+                              ]
+                            );
+                          }
+                        );
+                      }
+                    }
+                  )),
+                  new Container(height:30.0)
+                ]
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class Option extends StatefulWidget{
+  int position;
+  Option(this.position);
+  @override
+  OptionState createState() => new OptionState();
+}
+
+class OptionState extends State<Option>{
+  @override
+  Widget build(BuildContext context) {
+    return new Container(height: 50.0,padding: EdgeInsets.only(left:30.0,right:30.0),child: new TextField(
+      decoration: new InputDecoration(
+      hintText: 'Option '+widget.position.toString(),
+      filled: true,
+      fillColor: Colors.white,
+      border: InputBorder.none,
+      counterText: "",
+      ),
+      onSubmitted: (s){
+        CreatePollState.choices[widget.position-1] = s;
+      }
+    ));
+  }
+}
+
+class Settings extends StatefulWidget{
+  @override
+  SettingsState createState() => new SettingsState();
+}
+
+class SettingsState extends State<Settings>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Settings",style: new TextStyle(color:Colors.white)),backgroundColor: Colors.black54),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class SettingsInfo{
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return new File('$path/themeinfo.txt');
+  }
+
+  Future<List<String>> readData() async {
+    try {
+      final file = await _localFile;
+      String contents = await file.readAsString();
+
+      if(contents.split(" ").length!=1){
+        return null;
+      }
+
+      List<String> list = contents.split(" ");
+
+      return list;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<File> writeData(String data) async {
+    final file = await _localFile;
+    return file.writeAsString(data);
+  }
+
+}
