93c5597567aa0c58625ed842e45560793d5e118b
Blake Bottum <blakeplatypus@gmail.com>
2018-08-03 14:31:17 -0700

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1418,13 +1418,13 @@ class ImageViewState extends State<ImageView>{
               new Positioned(
                 right:10.0,
                 top:MediaQuery.of(context).padding.top,
-                child: new AnimatedOpacity(opacity:isAnimating?0.0:1.0,duration:new Duration(milliseconds:200),child:new IconButton(iconSize:30.0*(DefaultTextStyle.of(context).style.fontSize/48.0),color:Colors.white,icon:new Icon(Icons.close),onPressed:(){hasLeft = true;Navigator.of(context).pop();}))
+                child: new AnimatedOpacity(opacity:isAnimating?0.0:1.0,duration:new Duration(milliseconds:200),child:new IconButton(iconSize:30.0*MediaQuery.of(context).size.width/375.0,color:Colors.white,icon:new Icon(Icons.close),onPressed:(){hasLeft = true;Navigator.of(context).pop();}))
               ),
-              new IgnorePointer(child:new Positioned(
+              new Positioned(
                   left:15.0,
-                  top:MediaQuery.of(context).padding.top+5,
-                  child: new AnimatedOpacity(opacity:isAnimating?0.0:1.0,duration:new Duration(milliseconds:200),child:new Text(widget.name,style:new TextStyle(fontSize:DefaultTextStyle.of(context).style.fontSize/1.75,color:Colors.white)))
-              ))
+                  top:MediaQuery.of(context).padding.top+5*375.0/MediaQuery.of(context).size.width,
+                  child: new IgnorePointer(child:new AnimatedOpacity(opacity:isAnimating?0.0:1.0,duration:new Duration(milliseconds:200),child:new Text(widget.name,style:new TextStyle(fontSize:(48.0/1.75)*MediaQuery.of(context).size.width/375.0,color:Colors.white))))
+              )
               //new AnimatedOpacity(opacity:isAnimating?0.0:1.0,duration:new Duration(milliseconds:200),child:new Container(color:Colors.white70,height:MediaQuery.of(context).padding.top+kToolbarHeight,child:new AppBar(actions:[new IconButton(icon:new Icon(Icons.close),onPressed:(){hasLeft = true;Navigator.of(context).pop();})],automaticallyImplyLeading:false,centerTitle:false,title:new Text(widget.name,style:new TextStyle(color:Colors.white)),backgroundColor: Colors.transparent,elevation: 0.0)))
             ]:[
               widget.child
