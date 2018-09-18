2bc21f0bbd9e41c01d2dfbd951439b2201a7bd9b
Blake Bottum <blakeplatypus@gmail.com>
2018-09-18 18:40:12 -0500

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1438,17 +1438,15 @@ class ViewOrVoteState extends State<ViewOrVote>{
     colorStr = colorStr.replaceAll("#", "");
     int val = 0;
     int length = colorStr.length;
-    for (int i = 0; i < length; i++) {
+    for(int i = 0; i < length; i++){
       int hexDigit = colorStr.codeUnitAt(i);
-      if(hexDigit >= 48 && hexDigit <= 57) {
-        val += (hexDigit - 48) * (1 << (4 * (length - 1 - i)));
-      }else if (hexDigit >= 65 && hexDigit <= 70) {
-        // A..F
-        val += (hexDigit - 55) * (1 << (4 * (length - 1 - i)));
-      }else if (hexDigit >= 97 && hexDigit <= 102) {
-        // a..f
-        val += (hexDigit - 87) * (1 << (4 * (length - 1 - i)));
-      }else {
+      if(hexDigit>=48&&hexDigit<=57){
+        val+=(hexDigit-48)*(1<<(4*(length-1-i)));
+      }else if(hexDigit >= 65 && hexDigit <= 70){
+        val+=(hexDigit-55)*(1<<(4*(length-1-i)));
+      }else if(hexDigit >= 97 && hexDigit <= 102){
+        val+=(hexDigit-87)*(1<<(4*(length-1-i)));
+      }else{
         throw new FormatException("An error occurred when converting a color");
       }
     }
